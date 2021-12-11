b41db84baf70a54764c306d881d7d1e3f42173ba
Leonardo Camargo <leonardorcamargo@gmail.com>
2021-12-11 17:08:28 -0300

    Adicionado funcionalidades ao Todo List

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,26 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+    setTasks([...tasks, {
+      id: (new Date()).getTime(), 
+      title: newTaskTitle, 
+      isComplete: false
+    }]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => {
+      if (task.id === id) {
+        task.isComplete = true;
+      }
+      return task;
+    }))
   }
-
+  
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id));
   }
 
   return (
@@ -38,7 +49,7 @@ export function TaskList() {
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
-          <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
+          <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask} disabled={!newTaskTitle}>
             <FiCheckSquare size={16} color="#fff"/>
           </button>
         </div>
